495a3d500d078de630e6873735d4ad16a102b25c
borovkovaDasha <borovkova.dasha@gmail.com>
2016-05-24 00:31:20 +0300

    some changes

--- server.py
+++ server.py
@@ -3,6 +3,8 @@
 from threading import Timer
 from flask import Flask, jsonify, abort, request
 
+last_speed = []
+last_coordinate = []
 i = 0
 tasks = 0
 
@@ -22,17 +24,68 @@ tasks = [
 
 @app.route('/todo/api/v1.0/taskspost', methods=['POST'])
 def create_task():
+    #print(request.json['title'])
     if not request.json or not 'title' in request.json:
         abort(400)
     task = {
         'id': tasks[-1]['id'] + 1,
         'title': request.json['title'],
-        'description': request.json.get('description', ""),
+        'description': "description",
         'done': True
     }
+    print(request.json['title'])
     request_split(request.json['title'])
-    return jsonify({'done': True}), 200
+    print(x_coordinate)
+    string = "gps;" + x_coordinate + "," + y_coordinate + ";" + "speed;" + speed;
+    print(string)
+    return jsonify({'result': string}), 200
 
+def request_split(requestForSplit):
+    temp = requestForSplit.split(';')
+    string = []
+    timeForAcc = []
+    timeForCompas = []
+    acc = []
+    compas = []
+    count = 1
+    i = 0
+    for element in temp:
+        if temp[count] == "last_coordinates":
+            break        
+        string = temp[count].split(",")
+        for element in string:
+            if i == 0:
+                timeForAcc.append(string[i])
+            if i == 1 or i == 2 or i == 3:
+                acc.append(string[i])
+            if i == 4:
+                timeForCompas.append(string[i])
+            if i == 5:
+                compas.append(string[i])
+                i = 0
+                continue
+            i = i + 1
+        count = count + 1
+    last_coordinate = temp[count+1].split(",")
+    global x_coordinate
+    x_coordinate = str(last_coordinate[0])
+    global y_coordinate
+    y_coordinate = str(last_coordinate[1])
+    global speed
+    speed = str(temp[count+3])
+    print("timeForAcc")
+    print(timeForAcc)
+    print("acc")
+    print(acc)
+    print("timeForCompas")
+    print(timeForCompas)
+    print("compas")
+    print(compas)
+    print("x_coordinate")
+    print(x_coordinate)
+    print("y_coordinate")
+    print(y_coordinate)
+	
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
@@ -50,40 +103,6 @@ def test():
     return "<strong>It's Alive!</strong>"
 
 
-def request_split(requestForSplit):
-    temp = requestForSplit.split(',')
-    i = 1
-    count = 0
-    timeForAcc = []
-    timeForCompas = []
-    acc = []
-    compas = []
-    for element in temp:
-        if i == 1:
-            timeForAcc.append(temp[count])
-        if i == 2 or i == 3 or i == 4:
-            acc.append(temp[count])
-        if i == 5:
-            timeForCompas.append(temp[count])
-        if i == 6:
-            compas.append(temp[count])
-            i = 0
-        i = i + 1
-        count = count + 1
-
-def read_str(i):
-    if i < len(lines):
-        global str
-        str = lines[i]
-        tasks[1] = str
-        print(tasks[1])
-        t = Timer(2.0, lambda: read_str(i + 1))
-        t.start()
-    else:
-        print("end")
-
-def timer(i):
-    t = Timer(2.0, read_str(i)).start
 
 if __name__ == '__main__':
     app.run()

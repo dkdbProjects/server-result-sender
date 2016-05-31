0f9d4582b2e4e821ab891d8bcdf909d0f5464b8a
borovkovaDasha <borovkovaDasha@git.com>
2016-05-31 23:40:52 +0300

    some changes with position calculating

--- position.py
+++ position.py
@@ -6,13 +6,15 @@ import utm
 
 def calculate_position(speed,time,angle,x,y):
     i = 0
-    time_prev = 0
+    time_dif = time[len(time)-1] - time[0]
+    print("time")
+    print(time_dif)
     dif_x = 0
     dif_y = 0
     speed_av = sum(speed)/len(speed)
     angle_av = sum(angle)/len(angle)
-    dif_x = speed_av*5/18*math.sin(angle_av*math.pi/180)*(time)
-    dif_y = speed_av*5/18*math.cos(angle_av*math.pi/180)*(time)
+    dif_x = speed_av*5/18*math.sin(angle_av*math.pi/180)*(time_dif)
+    dif_y = speed_av*5/18*math.cos(angle_av*math.pi/180)*(time_dif)
     utmcoor = utm.from_latlon(x,y)
     print(utmcoor)
     newx = utmcoor[0] + dif_x;

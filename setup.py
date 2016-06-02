f43de58884a8776c3b0d91191e7d58ecd604cdd7
dkdbProjects <dkdbProjects@outlook.com>
2016-06-02 16:58:01 +0300

    quick fix

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(name='FlaskApp',
       install_requires=['Flask>=0.10.1'],
       install_requires=['Flask-MySQLdb>=0.2.0'],
       install_requires=['Flask-RESTful>=0.3.5'],
-      install_requires=['utm>=0.4.0']
-      install_requires=['numpy>=1.11.0']
-      install_requires=['scipy>=0.17.1']
-      install_requires=['scikit-learn>=0.17.1']
+      install_requires=['utm>=0.4.0'],
+      install_requires=['numpy>=1.11.0'],
+      install_requires=['scipy>=0.17.1'],
+      install_requires=['scikit-learn>=0.17.1'],
       install_requires=['matplotlib>=1.5.1']
      )

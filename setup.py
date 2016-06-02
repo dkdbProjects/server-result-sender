da5beabceb287b0dc8dfaf5137b6ffa162bd6c2b
dkdbProjects <dkdbProjects@outlook.com>
2016-06-02 17:57:45 +0300

    second small fix
    (will be merged with previous later)

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ setup(name='FlaskApp',
       author='Damir Kanafeev, Dasha Borovkova',
       author_email='domir332@live.ru',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask>=0.10.1'],
-      install_requires=['Flask-MySQLdb>=0.2.0'],
-      install_requires=['Flask-RESTful>=0.3.5'],
-      install_requires=['utm>=0.4.0'],
-      install_requires=['numpy>=1.11.0'],
-      install_requires=['scipy>=0.17.1'],
-      install_requires=['scikit-learn>=0.17.1'],
-      install_requires=['matplotlib>=1.5.1']
+      install_requires=['Flask>=0.10.1',
+                        'Flask-MySQLdb>=0.2.0',
+                        'Flask-RESTful>=0.3.5',
+                        'utm>=0.4.0',
+                        'numpy>=1.11.0',
+                        'scipy>=0.17.1',
+                        'scikit-learn>=0.17.1',
+                        'matplotlib>=1.5.1'
+      ]
      )
